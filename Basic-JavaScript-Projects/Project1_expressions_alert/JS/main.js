298989ad9f566253d49b8a3f7186dda76461c297
Neemag97 <71804843+Neemag97@users.noreply.github.com>
2020-10-19 03:58:58 -0700

    Create JavaScript folder

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,28 @@
+window.alert("Hello, World!"); //popsup a message on the window
+document.write('Hello, World!');
+
+var A = "This is a string";
+document.write(A);
+
+window.alert("variable").innerHTML =
+"The value of z is: " + z;
+var x = 5;
+var y = 6;
+var z = x + y;
+
+var x = "They are the so-called \"Pony\" from the north.";
+document.getElementById("demo").innerHTML = x;
+
+document.write(Start_by_doing_what_is_necessary, + then_do_what_is_possible, + suddenly_you_are_doing_the_impossible + -Francis_of_Assisi);
+
+var B = "Concatenated" + "string"
+document.write(B);
+
+var person = "John Doe", carName = "Volvo", price = 200;
+document.getElement(carName);
+
+var price = "I have $2000"
+var price = price.fontcolor("purple")
+document.write(price);
+
+document.write(3+3);
