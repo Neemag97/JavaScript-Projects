b7804c8de59acf77dd25e28bddc683dbf15da43a
Neemag97 <71804843+Neemag97@users.noreply.github.com>
2020-10-23 17:08:13 -0700

    finished project5

--- Basic-JavaScript-Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic-JavaScript-Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -7,23 +7,48 @@ function my_Function() {
     document.write("typeof");
 }
 
-function my__Function() {
     var x = 6;
     var y = 5;
     document.write (x + y);
+
+function infinity_Function() {  //infinity
+    document.getElementById("demo").innerHTML = 1.7976931348623157E+10308 + "<br>" +  -1.7976931348623157E+10308;
 }
 
-function myFunction() {
-    var res = "";
-    res = res + isNaN(0 / 0) + ": 0 / 0<br>";
-    res = res + isNaN(null) + ": null<br>";
-    document.getElementById("infinity").innerHTML = res; 
+console.log(2 + 2)
+
+var x=10;
+var y=2;
+document.write(y + x);
+
+document.write(10 < 2)
+
+document.write("10" + 2);
+
+document.write(10 == 10);
+
+x= 9;
+y=9;
+document.write(x === y);
+
+x = 82;
+y = "82";
+document.write(x === y);
+
+A = "Magnus";
+B = "Magnus";
+document.write(A === B);
+
+document.write(5 > 2 && 10 > 4); // AND written: && = true because five is greater than two, and ten is greater than four
+document.write(5 > 8 && 10 > 4); // AND written: && = false because five is less than eight, and ten is greater than four
+
+document.write(5 > 10 || 10 > 4); //OR written: || “true” because, while 5 is not greater than 10, 10 is greater than 4
+document.write(5 > 10 || 10 > 20); //OR written: || “fase” because, while 5 is not greater than 10, 10 is not greater than 20
+
+function not_Fuunction(){
+    document.getElementById("not").innerHTML= !(20 > 10); //Not operator "False" because 20 is truelly greater than 10.
 }
 
-function infinity_Function() {  
-    document.write (1.7976931348623157E+10308 + "<br>" +  -1.7976931348623157E+10308);
-  }
-
-  var x=10;
-  var y=2;
-  document.write(y + x);
+function not_Fuunction(){
+    document.getElementById("not").innerHTML= !(5 > 10); //Not operator "False" because 5 is truelly less than 10.
+}
