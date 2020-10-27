fb57bb444ea618a6ab6230635158e4aa7c7017eb
Neemag97 <71804843+Neemag97@users.noreply.github.com>
2020-10-27 09:02:41 -0700

    .

--- Basic-JavaScript-Projects/Project10_loops_arrays/JS/main.js
+++ Basic-JavaScript-Projects/Project10_loops_arrays/JS/main.js
@@ -1,4 +1,4 @@
-//Loop
+//For Loop
 var text = "";
 var i;
 for (i = 0; i < 5; i++) {
@@ -13,7 +13,7 @@ while (i < 10) {
   text += "<br>The number is " + i;
   i++;
 }
-document.getElementById("Loop").innerHTML = text;
+document.getElementById("wile_Loop").innerHTML = text;
 
 
 //ARRAY
@@ -28,10 +28,38 @@ function array_Function() {
 
 
 //CONST KEYWORD
-function constant_function() {
+function constant_Function() {
     const Musical_Instrument = {type: "guiter", brand:"Fender", color:"black"};
     Musical_Instrument.color= "blue";  //changed the “black” value for the color property to “blue”.
-    Musical_Instrument.price = "$875";
-    Musical_Instrument.weight = "10lb"
+    Musical_Instrument.price = "$900";
     document.getElementById("Constant").innerHTML= "The cost of the "  +  Musical_Instrument.type + Musical_Instrument.price;
 }
+
+//The let keyword
+var x = 82;
+document.write(x);
+{
+  let x = 33;
+  document.write("<br>" = x);
+}
+document.write("<br>" + x);
+
+var x = 82;
+document.write(x);
+{
+  var x =33;
+  document.write("<br>" + x);
+}
+document.write("<br>" + x);
+
+//OBJECTS
+let car = {
+  make = "Dodge",
+  model = "Viper",
+  year = "2021",
+  color ="red",
+  description : function() {
+    return "The car is a "  +  this.year  +  this.color  +  this.make  +  this.model;
+  }
+};
+document.getElementById("Car-Object")== captureEvents.description();
